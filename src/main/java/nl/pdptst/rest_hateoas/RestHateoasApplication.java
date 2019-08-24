f95cbcab7a4bf3b802b77081589911b49c59e7b9
binne101 <binne101@gmail.com>
2019-08-24 21:46:50 +0200

    PDP-8: sonar verwijder 2 unused imports, sonar moet nu slagen

--- src/main/java/nl/pdptst/rest_hateoas/RestHateoasApplication.java
+++ src/main/java/nl/pdptst/rest_hateoas/RestHateoasApplication.java
@@ -2,8 +2,8 @@ package nl.pdptst.rest_hateoas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.lang.String; //Code smell 1 for sonar
-import java.lang.Exception; //Code smell 2 for sonar
+//import java.lang.String; //Code smell 1 for sonar
+//import java.lang.Exception; //Code smell 2 for sonar
 
 @SpringBootApplication
 public class RestHateoasApplication {
